897823fb53cb1b437706d3a58cf425fd2916f0fc
artuuursliepins <artuuursliepins@gmail.com>
2025-03-16 16:33:55 +0200

    Update keep_alive.py

--- keep_alive.sh
+++ keep_alive.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-while true; do
-  curl -s https://lilypond-resources.onrender.com/ > /dev/null
-  sleep 300  # Ping ik pēc 5 minūtēm (300 sekundes)
-done
+import time
+import requests
+
+url = "https://lilypond-resources.onrender.com/"
+
+while True:
+    try:
+        response = requests.get(url)
+        print(f"Ping server: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+    
+    time.sleep(300)  # 5 minūšu pauze
