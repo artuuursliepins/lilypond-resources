a9dafd15f945020427928db9ed17febfa622f262
artuuursliepins <artuuursliepins@gmail.com>
2025-03-17 20:27:16 +0200

    Update lilypond_web.py

--- lilypond_web.py
+++ lilypond_web.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Meklē /templates/index.html
+  if os.path.exists("templates/index.html"):
+    return render_template('index.html')
+else:
+    return "HTML fails nav atrasts!", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
